cb9865f8a3b5518a74a41a6060899ec695671e71
level237 <Bramslevel129@gmail.com>
2022-03-06 12:18:21 +0100

    tailwindcss install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mon super site</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     @include('partials.navbar')
     @yield('content')
 </body>
+    <script src="{{asset('js/app.js')}}"></script>
 </html>
