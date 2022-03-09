153d1323fc6f520ee3fa13d69093756f835fc98b
level237 <Bramslevel129@gmail.com>
2022-03-09 20:59:36 +0100

    post comment display

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -6,5 +6,15 @@
 
     <h3>{{ $post-> content }}</h3>
 
+    {{-- @foreach ($post->comments as $comment)
+    <span>{{$comment->content}}</span>
+        
+    @endforeach --}}
+    @forelse ($post->comments as $comment)
+    <span>{{$comment->content}} | creer le {{$comment->created_at}}</span>
+    @empty
+        <span>Aucun commentaire</span>
+    @endforelse
+
 @endsection
 
