af762553cc1ded84b05491fa6d20330dff756865
level237 <Bramslevel129@gmail.com>
2022-03-29 15:48:32 +0100

    Required Validation

--- app/Http/Controllers/ArticleControllers.php
+++ app/Http/Controllers/ArticleControllers.php
@@ -37,11 +37,10 @@ class ArticleControllers extends Controller
         return view('form');
     }
     public function store(Request $request){
-        // $post=new Post();
-        // $post->title=$request->title;
-        // $post->content=$request->content;
-        // $post->save();
-        // dd('post créer');
+        $request->validate([
+            'title'=>'required',
+            'content'=>'required'
+        ]);
 
         Post::create([
             'title'=> $request->title,
